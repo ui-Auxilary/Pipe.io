033bfb3fa9efe8388849442271d5f62f04ecb14d
Keith Xiao <z5360635@ad.unsw.edu.au>
2023-10-05 16:58:20 +1100

    removed src folder

--- backend/src/error.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException
-
-class InputError(HTTPException):
-    code = 400
-    message = 'Input Error'
-
-class AccessError(HTTPException):
-    code = 403
-    message = 'Access Error'
