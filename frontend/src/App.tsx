123f52c239a3a762fe8e19528632b356eac960d4
KXiao <79000337+KXiao01@users.noreply.github.com>
2023-10-08 02:04:18 +1100

    Implemented frontened for authentication - Login, Logout, Register (#10)
    * initial commit - added static folderto backend to store default profile image
    
    * set profile img url to default image path
    
    * added react-router-dom to package.json
    
    * added react routers for login and register to app.tsx
    
    * Implemented login page
    
    * implemented register page
    
    * added logout and user profile photo to sidebar
    
    * uncommented window redirect after logging in
    
    * added redirect to login page if no token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,34 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login/";
+import Register from "./pages/Register";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Outlet} from "react-router-dom";
+
 
 export default function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    )
+  );
+
+
   return (
-    <>
-      <Home />
-    </>
+    <div className="App">
+      <RouterProvider router ={router} />
+    </div>
   );
 }
+
+
+const Root = () => {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,45 @@
 import S from "./style";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
+
 
 import Logo from "assets/logo.svg";
+
+
 
 export default function Sidebar() {
   return (
     <S.Container>
       <S.Logo src={Logo}></S.Logo>
+      <S.UserContainer>
+        <S.UserImg src="https://i.imgur.com/MjjdkFT.png"></S.UserImg>
+        <S.ButtonContainer>
+          <Button variant="outline-light" onClick={() => handleLogOut()}>Log Out</Button>
+        </S.ButtonContainer>
+      </S.UserContainer>
     </S.Container>
   );
+}
+
+
+
+function handleLogOut() {
+  fetch(`http://localhost:8000/users/logout`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+      "Authorization": "Bearer " + sessionStorage.getItem("token"),
+    },
+  })
+    .then((res) => {
+      if (res.status === 200) {
+        window.location.href = "/login";
+        sessionStorage.removeItem("token");
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      window.location.href = "/login";
+    });
+
 }
