bf1e1b41ef625c31a0582b2f97438e321dc41ca5
Mark <z5318362@ad.unsw.edu.au>
2023-10-08 16:43:53 +1100

    get_stock_data

--- backend/server/database.py
+++ backend/server/database.py
@@ -15,3 +15,4 @@ except Exception as e:
 
 db = client["Main"]
 pipes_collection = db["Pipes"]
+stock_collection = db["Stocks"]
--- backend/server/routers/pipes.py
+++ backend/server/routers/pipes.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 from server.models.pipes import Pipes
-from server.database import pipes_collection
+from server.database import pipes_collection, stock_collection
 from server.schemas.schemas import list_serial
+import yfinance as yf
+from datetime import datetime, timedelta
+import pandas as pd
 
 # Used for fetching Mongo objectID
 from bson import ObjectId
@@ -32,3 +35,26 @@ async def edit_pipe(id: str, pipe: Pipes):
 async def delete_pipe(id: str):
     pipes_collection.find_one_and_delete(
         {"_id": ObjectId(id)})
+
+@router.get('/get_stock_data/{stock_name}')
+async def get_stock_data(stock_name:str):
+    # Fetch data for the selected stock here
+    try:
+        stock_data = yf.Ticker(stock_name)
+        stock_data = stock_data.history()
+    except:
+        print("Stock name is wrong")
+        return
+    # Process and return the data
+    if not isinstance(stock_data, pd.DataFrame):
+        stock_data = pd.DataFrame(stock_data)
+    stock_data['stock_name'] = stock_name
+    stock_data = stock_data.to_dict(orient='records')
+    stock_collection.insert_many(stock_data)
+#@router.get('/get_stock_data/{stock_name}')
+#async def get_stock_data(stock_name: str):
+#    for stock in stock_collection.find({"stock_name": stock_name}):
+        
+        
+    
+    
