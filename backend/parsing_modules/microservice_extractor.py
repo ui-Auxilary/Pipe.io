01e189442853a8df5de029605e69a6e120a24810
Dhruv Agrawal <dhruv.gravity@gmail.com>
2023-10-10 07:12:03 +1100

    fixed up styling for existing microservices

--- backend/parsing_modules/microservice_extractor.py
+++ backend/parsing_modules/microservice_extractor.py
@@ -10,42 +10,46 @@ print(sys.path)
 
 
 def extract_microservice(python_file=None) -> str:
-    if (python_file):
-        print("FILE", python_file)
-        # load the module
-        imported_module = importlib.import_module(python_file)
+    if not python_file:
+        return ""
 
-        # create a json object to store the microservice information
-        microservice_json = {}
-        microservice_json['microservices'] = []
+    print("FILE", python_file)
+    # load the module
+    imported_module = importlib.import_module(python_file)
 
-        # get each of the microservice names
-        microservice_names = []
-        for service in dir(imported_module):
-            if not re.match(r'__.*__', service):
-                # check if callable
-                if callable(getattr(imported_module, service)):
-                    microservice_names.append(service)
+    # create a json object to store the microservice information
+    microservice_json = {}
+    microservice_json['microservices'] = []
 
-        for microservice in microservice_names:
-            microservice_function = getattr(imported_module, microservice)
-            microservice_signature = inspect.signature(microservice_function)
-            microservice_code = inspect.getsource(microservice_function)
+    # get each of the microservice names
+    microservice_names = []
+    for service in dir(imported_module):
+        if not re.match(r'__.*__', service):
+            # check if callable
+            if callable(getattr(imported_module, service)):
+                microservice_names.append(service)
 
-            params = []
-            for param in microservice_signature.parameters.values():
-                if param.default is param.empty:
-                    params.append(param.name)
-                else:
-                    params.append(f'{param.name}={param.default}')
+    for microservice in microservice_names:
+        microservice_function = getattr(imported_module, microservice)
+        microservice_signature = inspect.signature(microservice_function)
+        microservice_code = inspect.getsource(microservice_function)
 
-            doc = inspect.getdoc(microservice_function)
-            microservice_json['microservices'].append({
-                'name': microservice,
-                'doc': doc,
-                'parameters': params,
-                'code': microservice_code
-            })
+        params = []
+        for param in microservice_signature.parameters.values():
+            if param.default is param.empty:
+                params.append(param.name)
+            else:
+                params.append(f'{param.name}={param.default}')
 
-        return json.dumps(microservice_json, indent=4)
-    return ""
+        doc = inspect.getdoc(microservice_function)
+        microservice_json['microservices'].append({
+            'name': microservice,
+            'doc': doc,
+            'parameters': params,
+            'code': microservice_code
+        })
+
+    # save the file under microservices directory
+    #with open(f'./data/microservices/{python_file}', 'w') as f:
+
+    return json.dumps(microservice_json, indent=4)
