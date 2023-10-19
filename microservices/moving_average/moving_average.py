8a65b4ab34de632515a7d414c3b622e43e374457
Mark <z5318362@ad.unsw.edu.au>
2023-10-19 16:19:06 +1100

    change parameter default

--- microservices/moving_average/moving_average.py
+++ microservices/moving_average/moving_average.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import os
 import datetime
-def moving_avergae(input_file_path, output_file_path, window_size, date_column, value_column):
+def moving_avergae(input_file_path: str = 'stock_data.csv', output_file_path:str = 'moving_average.csv', window_size: int = 5, date_column:str = 'Date', value_column:str = 'Volume'):
     """get csv file path, output file path and window size
 
     Args:
         input_file_path (str): input file path
         output_file_path (str): output file path
         window_size (num): the window size of moving average
-        date_column (str): date column
-        value_column (str): value column
+        date_column (str): the colunm name of the date column in the input file
+        value_column (str): the colunm name of the value column in the output file
     Returns:
         pd.DataFrame: Dataframe from the csv file
     """
     
     df = pd.read_csv(input_file_path)
     df = df.sort_values(by=[date_column])
-    #df['Volume'] = df['Volume'].str.replace(',', '').astype(float)  # Remove commas and convert to float
     data_type = df[value_column].dtype
     if data_type == 'object':
         df[value_column] = df[value_column].str.replace(',', '').astype(float)
@@ -24,4 +23,3 @@ def moving_avergae(input_file_path, output_file_path, window_size, date_column, 
     df.to_csv(output_file_path, index=False)
     return df
     
-#moving_avergae("microservices/moving_average/stock_data.csv", "microservices/moving_average/mv.csv", 5, "Date", "Open")
