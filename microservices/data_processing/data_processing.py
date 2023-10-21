73b74fc62a064aadb021d8f74047ce9f0115d907
Josh Chen <z5358677@unsw.edu.au>
2023-10-22 01:00:18 +1100

    merge

--- microservices/data_processing/data_processing.py
+++ microservices/data_processing/data_processing.py
@@ -6,7 +6,7 @@ def drop_column(input_file_path: str = 'stock_data.csv', output_file_path: str =
     Args:
         input_file_path (str, optional): path for the input csv. Defaults to 'stock_data.csv'.
         output_file_path (str, optional): path for the output csv. Defaults to 'stock_data.csv'.
-        columns (str, optional): comma seperated column names to be droppeed. Defaults to ''.
+        columns (str, optional): comma seperated column names to be dropped. Defaults to ''.
 
     Returns:
         pd.DataFrame: Dataframe from the csv file
@@ -21,7 +21,7 @@ def drop_column(input_file_path: str = 'stock_data.csv', output_file_path: str =
             pass
     
     df.to_csv(output_file_path)
-    return df
+    return df.to_json()
 
 def sort_column(input_file_path: str = 'stock_data.csv', output_file_path: str = 'stock_data.csv', columns: str = ''):
     """Sorts csv on specified column
@@ -54,4 +54,26 @@ def sort_column(input_file_path: str = 'stock_data.csv', output_file_path: str =
         df = df.sort_values(by=column_list, ascending=ascending_list)
     
     df.to_csv(output_file_path)
-    return df
+    return df.to_json()
+
+def merge_csv(input_file_path_1: str = 'stock_data.csv', input_file_path_2: str = 'stock_data_2.csv', output_file_path: str = 'stock_data.csv', how: str = 'inner', left_on: str ='Date', right_on: str ='Date'):
+    """Merges csv using pandas.DataFrame.merge
+
+    Args:
+        input_file_path_1 (str, optional): path for the first input csv. Defaults to 'stock_data.csv'.
+        input_file_path_2 (str, optional): path for the second input csv. Defaults to 'stock_data_2.csv'.
+        output_file_path (str, optional): path for the output csv. Defaults to 'stock_data.csv'.
+        how (str, optional): Type of merge, as defined by df.merge. Defaults to 'inner'.
+        left_on (str, optional): left key of merge, as defined by df.merge. Defaults to 'Date'.
+        right_on (str, optional): right key of merge, as defined by df.merge. Defaults to 'Date'.
+
+    Returns:
+        pd.DataFrame: Dataframe from the csv file
+    """
+    df1 = pd.read_csv(input_file_path_1, index_col=0)
+    df2 = pd.read_csv(input_file_path_2, index_col=0)
+    
+    df3 = df1.merge(df2, left_on = left_on, right_on = right_on)
+
+    df3.to_csv(output_file_path)
+    return df3.to_json()
