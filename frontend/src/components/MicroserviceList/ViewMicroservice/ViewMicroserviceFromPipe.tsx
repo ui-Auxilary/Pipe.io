dd1cf1db509ea9935a0f011c861e8c4396224197
jchen4230 <z5358677@unsw.edu.au>
2023-10-25 15:03:27 +1100

    Feature/scrum 105/edit params (#33)
    * drop and teest
    
    * data dropping and sorting and tests
    
    * te
    
    * merge
    
    * plot data microservice
    
    * frontend of edit params
    
    * trying to get microservice id
    
    * Update z5358677.txt
    
    * updating parameters

--- frontend/src/components/MicroserviceList/ViewMicroservice/ViewMicroserviceFromPipe.tsx
+++ frontend/src/components/MicroserviceList/ViewMicroservice/ViewMicroserviceFromPipe.tsx
@@ -1,13 +1,23 @@
-import { useFormData } from 'components/MultiStepForm/Form/FormProvider'
 import S from './styles'
 import axios from "axios";
 import { useEffect, useState } from 'react'
 import Microservice from 'components/Microservice';
 
-export default function ViewMicroservice() {
-    const { microserviceData } = useFormData()
-    const { microservices } = microserviceData
+interface Props {
+    pipeId: string
+  }
 
+export default function ViewMicroserviceFromPipe({ pipeId }: Props) {
+    const [microservices, setMicroservices] = useState([]);
+
+    useEffect(() => {
+        console.log(pipeId)
+        axios.get(`http://localhost:8000/pipes/${pipeId}`).then(res => {
+            
+            setMicroservices(res.data.microservices)
+        })
+
+    }, [])
     let len = microservices ? (microservices as []).length : 0
     return (
         <S.Wrapper>
@@ -16,8 +26,7 @@ export default function ViewMicroservice() {
                 <span style={{ color: "#907F7F", fontWeight: 500 }}>Found {len} microservice(s)</span>
                 <S.Scrollbar>
                     {microservices && microservices.map(({ code, doc, name, parameters, parent_file }, index) => {
-                        console.log('hey loop')
-                        return <Microservice code={code} docstring={doc} name={name} param={parameters} parent_file={parent_file} from_pipe={false} />
+                        return <Microservice code={code} docstring={doc} name={name} param={parameters} parent_file={parent_file} from_pipe={true} parent_pipe_id={pipeId}/>
                     })}
                 </S.Scrollbar>
             </S.Container>
