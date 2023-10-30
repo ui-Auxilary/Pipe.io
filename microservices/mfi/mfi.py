78282d59ce3b3aa4e8d06fbc3d0e8302e2a08f8f
Mark <z5318362@ad.unsw.edu.au>
2023-10-30 20:30:09 +1100

    add test for mfi

--- microservices/mfi/mfi.py
+++ microservices/mfi/mfi.py
@@ -1,6 +1,22 @@
 import pandas as pd
 import os
 def calculate_mfi(input_file_path: str ="stock_data.csv", output_file_path:str = "mfi.csv", window_size: int =14, high_column: str = "High", low_column: str = "Low", close_column: str = "Close", value_column: str = "Volume", date_column: str = "Date"):
+    
+    """get csv file path, output file path and window size
+
+    Args:
+        input_file_path (str): input file path
+        output_file_path (str): output file path
+        window_size (num): the window size of moving average
+        high_column (str): the column name of the high value column
+        low_column (str): the column name of the low value column
+        close_column (str): the column name of the close
+        date_column (str): the colunm name of the date column in the input file
+        value_column (str): the colunm name of the value column in the output file
+    Returns:
+        pd.DataFrame: Dataframe from the csv file
+    """
+    
     input_file_path = os.path.join('/backend/data/', input_file_path)
     df = pd.read_csv(input_file_path)
     df = df.sort_values(by=[date_column])
