9dc80fd1b624f881d3f39f98c2ba46ce59280b95
Min Jun Xiao <minjunxiao1@gmail.com>
2023-10-30 19:37:40 +1100

    simpliefied code to map out microervice items

--- frontend/src/components/Microservice/Microservice.tsx
+++ frontend/src/components/Microservice/Microservice.tsx
@@ -24,17 +24,9 @@ export default function Microservice({ code, name, docstring, param, parent_file
   const handleCodeShow = (e: React.SyntheticEvent<EventTarget>) => { e.preventDefault(); setCode(true) };
 
   param && console.log('ITEMS', param)
-  
   const items = param && Object.keys(param).map((el) => {
-    let value = "";
-    if (param[el]["value"] != undefined) {
-      value = param[el]["value"];
-    } else if (param[el]["default"]) {
-      value = param[el]["default"];
-    }
-    return ({ label: el, type: "edit_param", name: id, value: value, elType: param[el] ? param[el]["type"] : '' })
+    return ({ label: el, type: "edit_param", name: id, value: param[el]["value"] ? param[el]["value"] : param[el]["default"] || '', elType: param[el] ? param[el]["type"] : '' })
   })
-  
 
   const tagOptions = [
     { value: 'value', label: 'Value' },
