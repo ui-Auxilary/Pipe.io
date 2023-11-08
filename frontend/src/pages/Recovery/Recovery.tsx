3210decd460f12e998c2feb4aa3d961d380c6051
Keith Xiao <z5360635@ad.unsw.edu.au>
2023-11-08 23:48:10 +1100

    added success screen to recovery page

--- frontend/src/pages/Recovery/Recovery.tsx
+++ frontend/src/pages/Recovery/Recovery.tsx
@@ -1,27 +1,23 @@
-  import { useState } from "react";
-  import { Form, Button, Card } from "react-bootstrap";
-  import Logo from "assets/logo.svg";
+import { useState } from "react";
+import { Form, Button, Card } from "react-bootstrap";
+import Logo from "assets/logo.svg";
+import Sent from "./components/Sent";
+import s from "./Style";
+import axios from "axios";
 
-  import s from "./Style";
 
-
-  export default function Recovery() {
+export default function Recovery() {
 
   const [values, setValues] = useState({
     email: '',
-    username: '',
-    password: '',
-    password2: '',
   });
-
-
+  const [isSubmit, setIsSubmit] = useState(false);
   const [errorMsg, seterrorMsg] = useState({
     message: '',
   })
 
   // State to check form validity
   const [valid, setValid] = useState(true)
-
 
   const handleEmailInput = (event: Event) => {
     setValues({ ...values, email: event.target.value })
@@ -35,41 +31,63 @@
 
   // Handle form submission
   const handleSubmit = (event: Event) => {
-
+    event.preventDefault();
+    setIsSubmit(true);
+    // Check if email is valid
+    if (!validateEmail(values.email)) {
+      seterrorMsg({ message: 'Please enter a valid email address.' })
+      setValid(false)
+      return;
+    }
+    // If email is valid
+    const data = new URLSearchParams();
+    data.append('email', values.email);
+    axios.post(`http://localhost:8000/users/forgot-password`, data)
+      .then(res => {
+        if (res.status === 200) {
+          setIsSubmit(true);
+        }
+      }).catch(err => {
+        console.error(err);
+      })
   }
 
   return (
+    <>
+    {!isSubmit && 
     <s.Container>
       <s.Logo src={Logo}></s.Logo>
-        <Card className="register-card">
-          <Card.Header><h5>Forgot Password</h5></Card.Header>
-          <Card.Body>
-            <Form className="register-form" onSubmit={handleSubmit}>
-              {!valid ? <div style={{ color: 'red' }}>{errorMsg.message}</div> : null}
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Enter email"
-                  name="email"
-                  value={values.email}
-                  onChange={handleEmailInput}
-                />
-                <Form.Text className="text-muted"> We'll send a verification code to your email. </Form.Text>
-              </Form.Group>
-              <s.ButtonContainer>
-                <Button variant="primary" type="submit">
-                  Next
-                </Button>
-              </s.ButtonContainer>
-              <s.ButtonContainer>
-                <Button variant="secondary" onClick={() => window.location.href="/login"}>
-                  Back
-                </Button>
-              </s.ButtonContainer>
-            </Form>
-          </Card.Body>
-        </Card>
-    </s.Container>
+      <s.CardContainer className="register-card">
+        <s.CardHeader><h5>Forgot Password</h5></s.CardHeader>
+        <s.CardBody>
+          <Form className="register-form" onSubmit={handleSubmit}>
+            {!valid ? <div style={{ color: 'red' }}>{errorMsg.message}</div> : null}
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={values.email}
+                onChange={handleEmailInput}
+              />
+              <Form.Text className="text-muted"> We'll send a reset link to your email.</Form.Text>
+            </Form.Group>
+            <s.ButtonContainer>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </s.ButtonContainer>
+            <s.ButtonContainer>
+              <Button variant="secondary" onClick={() => window.location.href="/login"}>
+                Back to Login
+              </Button>
+            </s.ButtonContainer>
+          </Form>
+        </s.CardBody> 
+      </s.CardContainer>
+    </s.Container>}
+    {isSubmit && <Sent email={values.email}/>}
+    </>
   );
-  }
+}
