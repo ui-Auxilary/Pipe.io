8ac8c243edb20cc1d90bff15a03bd7c13b571fe5
ZekaiMark <145641181+ZekaiMark@users.noreply.github.com>
2023-11-14 01:56:43 +1100

    add moving_average_test (#52)
    * add moving_average_test
    
    * add validation test for moving average
    
    * add validation check in moving average
    
    * add tests for moving average and rsi
    
    * add rsi test
    
    * add data fodlers
    
    * removed init.py
    
    * added init.py files
    
    ---------
    
    Co-authored-by: Min Jun Xiao <minjunxiao1@gmail.com>

--- microservices/data_processing/moving_average.py
+++ microservices/data_processing/moving_average.py
@@ -15,9 +15,22 @@ def moving_average(input_file_path: str = 'stock_data.csv', output_file_path:str
         pd.DataFrame: Dataframe from the csv file
     """
     
+    if not input_file_path.lower().endswith('.csv') or not output_file_path.lower().endswith('.csv'):
+        raise ValueError("Input and output file paths must have .csv extension")
+    
+    if not os.path.exists(input_file_path):
+        raise FileNotFoundError(f"Input file not found: {input_file_path}")
+
     df = pd.read_csv(input_file_path)
+    
+    if date_column not in df.columns or value_column not in df.columns:
+        raise ValueError(f"Columns {date_column} and {value_column} must exist in the input dataframe")
+    
+    if not isinstance(window_size, int):
+        raise ValueError("Window size must be an integer")
+    if window_size > len(df):
+        raise ValueError("Window size cannot be greater than the number of rows in the dataframe")
     df = df.sort_values(by=[date_column])
     df['moving_average'] = df[value_column].rolling(window=window_size).mean()
     df.to_csv(output_file_path)
     return df.to_json()
-
