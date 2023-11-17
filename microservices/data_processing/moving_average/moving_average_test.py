2eeeb4dc9fb7482bb0ff014f3e47753558a7821b
Mark <z5318362@ad.unsw.edu.au>
2023-11-17 20:46:07 +1100

    finish moving average and rsi test'

--- microservices/data_processing/moving_average/moving_average_test.py
+++ microservices/data_processing/moving_average/moving_average_test.py
@@ -1,68 +1,47 @@
 import pytest
 import pandas as pd
 import os
-from microservices.data_processing.moving_average.moving_average import moving_average
-
-def test_moving_average_basic():
-    sample_data = {
+from moving_average import moving_average
+import numpy as np
+sample_data = {
         'Date': ['2023-11-01', '2023-11-02', '2023-11-03', 
                  '2023-11-04', '2023-11-05'],
         'Volume': [100, 200, 300, 400, 500]
     }
-    sample_df = pd.DataFrame(sample_data)
-    sample_df.to_csv('sample_stock_data.csv')
+sample_df = pd.DataFrame(sample_data)
+sample_df.to_csv('sample_stock_data.csv')
+def test_moving_average_basic():
+    
     moving_average('sample_stock_data.csv', 'sample_moving_average.csv', 
                    3, "Date", "Volume")
     output_df = pd.read_csv('sample_moving_average.csv')
-    assert('moving_average' in output_df.columns)
-    assert (output_df['moving_average'].tolist() == [None, None, 200, 
-                                                     300, 400])
-    os.remove('sample_stock_data.csv')
+    assert('Moving Average' in output_df.columns)
+    expected_values = [np.nan, np.nan, 200.0, 300.0, 400.0]
+    np.testing.assert_allclose(output_df['Moving Average'].tolist(), expected_values, equal_nan=True)
+    #os.remove('sample_stock_data.csv')
     os.remove('sample_moving_average.csv')
-
-def test_moving_average_window_size():
+def test_moving_average_hard():
     sample_data = {
-        'Date': ['2023-11-01', '2023-11-02', '2023-11-03', 
-                 '2023-11-04', '2023-11-05'],
-        'Volume': [100, 150, 200, 250, 300]
+        'Date': ['2023-11-01', 
+                 '2023-11-04','2023-11-03' ,'2023-11-05', '2023-11-02'],
+        'Volume': [200, 100, 80, 500, 10]
     }
     sample_df = pd.DataFrame(sample_data)
-    sample_df.to_csv('sample_stock_data.csv')
-    moving_average('sample_stock_data.csv', 
-                   'sample_moving_average.csv', 2, "Date", "Volume")
-    output_df = pd.read_csv('sample_moving_average.csv')
-    assert('moving_average' in output_df.columns)
-    assert (output_df['moving_average'].tolist() == [None, 125 , 175, 
-                                                     225 , 275])
-    os.remove('sample_stock_data.csv')
-    os.remove('sample_moving_average.csv')
-
-
-def test_moving_average_non_increasing():
-    sample_data = {
-        'Date': ['2023-11-01', '2023-11-02', '2023-11-03', 
-                 '2023-11-04', '2023-11-05'],
-        'Volume': [100, 200, 100, 200, 100]
-    }
-    sample_df = pd.DataFrame(sample_data)
-    sample_df.to_csv('sample_stock_data.csv')
-    moving_average('sample_stock_data.csv', 
-                   'sample_moving_average.csv', 1, "Date", "Volume")
-    output_df = pd.read_csv('sample_moving_average.csv')
-    assert('moving_average' in output_df.columns)
-    assert (output_df['moving_average'].tolist() == [100, 200, 100, 200, 100])
-    os.remove('sample_stock_data.csv')
-    os.remove('sample_moving_average.csv')
+    sample_df.to_csv('sample_stock_data2.csv')
+    moving_average('sample_stock_data2.csv', 'sample_moving_average2.csv', 
+                   2, "Date", "Volume")
+    output_df = pd.read_csv('sample_moving_average2.csv')
+    assert('Moving Average' in output_df.columns)
+    expected_values = [np.nan, 105.0, 45.0, 90.0, 300.0]
+    np.testing.assert_allclose(output_df['Moving Average'].tolist(), expected_values, equal_nan=True)
+    os.remove('sample_stock_data2.csv')
+    os.remove('sample_moving_average2.csv')
 
 
 def test_invalid_file_extension():
     with pytest.raises(ValueError) as exc_info:
-        moving_average(input_file_path='invalid_input.txt',
-                        output_file_path='invalid_output.txt', 
-                        window_size=3, date_column='Date', 
-                        value_column='Volume')
-    assert str(exc_info.value) == """Input and output file paths 
-                                    must have .csv extension"""
+        moving_average('invalid_input.txt','invalid_output.txt', 3, 'Date', 'Volume')
+    assert str(exc_info.value) == "Input and output file paths must have .csv extension"
 
 
 def test_nonexistent_file():
@@ -74,38 +53,32 @@ def test_nonexistent_file():
 
 
 def test_missing_columns():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        moving_average(input_file_path=input_file_path, 
-                       output_file_path=output_file_path, 
-                       window_size=3, date_column='Date', 
-                       value_column='InvalidColumn')
-    assert str(exc_info.value) == "InvalidColumn"
+        moving_average(input_file_path, output_file_path, 3, 'Date', 'InvalidColumn')
+    assert str(exc_info.value) == "Columns Date and InvalidColumn must exist in the input dataframe"
 
 
 def test_non_integer_window_size():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        moving_average(input_file_path=input_file_path, 
-                       output_file_path=output_file_path, window_size=3.5, 
-                       date_column='Date', value_column='Volume')
+        moving_average(input_file_path, output_file_path, 3.5, 'Date', 'Volume')
     assert str(exc_info.value) == "Window size must be an integer"  
 
 
 def test_large_window_size():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        moving_average(input_file_path=input_file_path, 
-                       output_file_path=output_file_path, window_size=100, 
-                       date_column='Date', value_column='Volume')
-    assert str(exc_info.value) == """Window size cannot be greater 
-                                than the number of rows in the dataframe"""
+        moving_average(input_file_path, output_file_path, 100, 'Date', 'Volume')
+    assert str(exc_info.value) == """Window size cannot be greater than the number of rows in the dataframe"""
 
 
 def test_string_window_size():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        moving_average(input_file_path=input_file_path, 
-                       output_file_path=output_file_path, window_size="Hello", 
-                       date_column='Date', value_column='Volume')
+        moving_average(input_file_path, output_file_path, "Hello", 'Date', 'Volume')
     assert str(exc_info.value) == "Window size must be an integer"
--- microservices/data_processing/rsi/rsi_test.py
+++ microservices/data_processing/rsi/rsi_test.py
@@ -2,60 +2,86 @@ import pandas as pd
 import os
 import pytest
 from rsi import calculate_rsi
-@pytest.fixture
-def test_rsi():
-
-    sample_data = {
-        'Date': ['2023-11-01', '2023-11-02', '2023-11-03', '2023-11-04', '2023-11-05'],
+import numpy as np
+sample_data = {
+        'Date': ['2023-11-01', '2023-11-02', '2023-11-03', 
+                 '2023-11-04', '2023-11-05'],
         'Open': [100, 200, 300, 400, 500]
     }
-    sample_df = pd.DataFrame(sample_data)
-    sample_df.to_csv('sample_stock_data.csv')
+sample_df = pd.DataFrame(sample_data)
+sample_df.to_csv('sample_stock_data.csv')
+def test_rsi():
     calculate_rsi('sample_stock_data.csv', 'sample_rsi.csv', 3, "Date", "Open")
     output_df = pd.read_csv('sample_rsi.csv')
     assert('RSI' in output_df.columns)
-    assert (output_df['RSI'].tolist() == [None, None, 100, 100, 100])
-    os.remove('sample_stock_data.csv')
-    os.remove('sample_rsi.csv')
+    expected_values = [np.nan, np.nan, 100.0, 100.0, 100.0]
+    np.testing.assert_allclose(output_df['RSI'].tolist(), expected_values, equal_nan=True)
 def test_rsi2():
     
     sample_data = {
         'Date': ['2023-11-01', '2023-11-02', '2023-11-03', '2023-11-04', '2023-11-05'],
-        'Open': [100, 150, 200, 250, 300]
+        'Open': [22, 44, 33, 88, 77]
     }
     sample_df = pd.DataFrame(sample_data)
-    sample_df.to_csv('sample_stock_data.csv')
-    calculate_rsi('sample_stock_data.csv', 'sample_rsi.csv', 1, "Date", "Open")
-    output_df = pd.read_csv('sample_rsi.csv')
+    sample_df.to_csv('sample_stock_data2.csv')
+    calculate_rsi('sample_stock_data2.csv', 'sample_rsi2.csv', 2, "Date", "Open")
+    output_df = pd.read_csv('sample_rsi2.csv')
     assert('RSI' in output_df.columns)
-    assert (output_df['RSI'].tolist() == [None, 100, 100, 100, 100])
-    os.remove('sample_stock_data.csv')
-    os.remove('sample_rsi.csv')
+    expected_values = [np.nan, 100.0, 66.66666666666666, 83.33333333333333, 83.33333333333333]
+    np.testing.assert_allclose(output_df['RSI'].tolist(), expected_values, equal_nan=True)
+def test_rsi3():
+    
+    sample_data = {
+        'Date': ['2023-11-01', '2023-11-02', '2023-11-03', '2023-11-04', '2023-11-05', '2023-11-06', '2023-11-07'],
+        'Open': [10, 58, 33, 62, 77, 108, 19]
+    }
+    sample_df = pd.DataFrame(sample_data)
+    sample_df.to_csv('sample_stock_data3.csv')
+    calculate_rsi('sample_stock_data3.csv', 'sample_rsi3.csv', 2, "Date", "Open")
+    output_df = pd.read_csv('sample_rsi3.csv')
+    assert('RSI' in output_df.columns)
+    expected_values = [np.nan, 100.0, 65.753425, 53.703704, 100.0, 100.0, 25.833333]
+    np.testing.assert_allclose(output_df['RSI'].tolist(), expected_values, equal_nan=True)
 def test_invalid_file_extension():
     with pytest.raises(ValueError) as exc_info:
-        calculate_rsi(input_file_path='invalid_input.txt', output_file_path='invalid_output.txt', window_size=3, date_column='Date', value_column='Open')
-    assert str(exc_info.value) == "Input and output file paths must have .csv extension"  
+        calculate_rsi('invalid_input.txt','invalid_output.txt', 3, 'Date', 'Volume')
+    assert str(exc_info.value) == "Input and output file paths must have .csv extension"
+
+
 def test_nonexistent_file():
     with pytest.raises(FileNotFoundError) as exc_info:
-        calculate_rsi(input_file_path='nonexistent_file.csv', output_file_path='nonexistent_output.csv', window_size=3, date_column='Date', value_column='Open')
+        calculate_rsi('nonexistent_file.csv', 'nonexistent_output.csv', 3, 'Date', 'Open')
     assert str(exc_info.value) == f"Input file not found: nonexistent_file.csv"
+
+
 def test_missing_columns():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        calculate_rsi(input_file_path=input_file_path, output_file_path=output_file_path, window_size=3, date_column='Date', value_column='InvalidColumn')
-    assert str(exc_info.value) == "InvalidColumn" 
+        calculate_rsi(input_file_path, output_file_path, 3, 'Date', 'InvalidColumn')
+    assert str(exc_info.value) == "Columns Date and InvalidColumn must exist in the input dataframe"
+
+
 def test_non_integer_window_size():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        calculate_rsi(input_file_path=input_file_path, output_file_path=output_file_path, window_size=3.5, date_column='Date', value_column='Open')
-    assert str(exc_info.value) == "Window size must be an integer"
+        calculate_rsi(input_file_path, output_file_path, 3.5, 'Date', 'Open')
+    assert str(exc_info.value) == "Window size must be an integer"  
+
+
 def test_large_window_size():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        calculate_rsi(input_file_path=input_file_path, output_file_path=output_file_path, window_size=100, date_column='Date', value_column='Open')
-    assert str(exc_info.value) == "Window size cannot be greater than the number of rows in the dataframe"
+        calculate_rsi(input_file_path, output_file_path, 100, 'Date', 'Open')
+    assert str(exc_info.value) == """Window size cannot be greater than the number of rows in the dataframe"""
+
+
 def test_string_window_size():
-    input_file_path, output_file_path = "stock_data.csv"
+    input_file_path = "sample_stock_data.csv"
+    output_file_path = "sample_moving_average.csv"
     with pytest.raises(ValueError) as exc_info:
-        calculate_rsi(input_file_path=input_file_path, output_file_path=output_file_path, window_size="Hello", date_column='Date', value_column='Volume')
+        calculate_rsi(input_file_path, output_file_path, "Hello", 'Date', 'Open')
     assert str(exc_info.value) == "Window size must be an integer"
+
